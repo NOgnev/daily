e66a7675f8483cb0090c773a9ffd0096577cd068
n.ognev <n.ognev@tinkoff.ru>
2025-04-25 16:43:36 +0300

    collapseOnSelect fix

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,15 +12,15 @@ const Header = () => {
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg" >
       <Container>
         <Navbar.Brand as={Link} to="/">My App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/about" eventKey="i">About</Nav.Link>
             {isAuthenticated && (
-              <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+              <Nav.Link as={Link} to="/profile" eventKey="i">Profile</Nav.Link>
             )}
           </Nav>
           <Nav>
@@ -28,8 +28,8 @@ const Header = () => {
               <Button variant="outline-light" onClick={handleLogout}>Logout</Button>
             ) : (
               <>
-                <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                <Nav.Link as={Link} to="/login" eventKey="i">Login</Nav.Link>
+                <Nav.Link as={Link} to="/register" eventKey="i">Register</Nav.Link>
               </>
             )}
           </Nav>
