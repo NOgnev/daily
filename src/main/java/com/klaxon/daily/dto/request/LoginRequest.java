89ad1510ab39f9e835a347c3e4dfa7024219b1be
n.ognev <n.ognev@tinkoff.ru>
2025-06-15 15:41:46 +0300

    fix

--- src/main/java/com/klaxon/daily/dto/request/LoginRequest.java
+++ src/main/java/com/klaxon/daily/dto/request/LoginRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 
 public record LoginRequest(@NotBlank(message = "Nickname is required")
                            @Size(min = 3, max = 20, message = "Nickname must be between 3 and 20 characters")
-                           @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Nickname can contain only letters, digits, and underscores")
+                           @Pattern(regexp = "^[a-zA-Z0-9_.]+$", message = "Nickname can contain only letters, digits, and underscores")
                            String nickname,
                            @NotBlank(message = "Password is required")
                            @Hidden
